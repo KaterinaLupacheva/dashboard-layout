09cbaa6a9183eabfe703187dac8078e5bc8420f8
KaterinaLupacheva <katerinalupacheva@gmail.com>
2021-01-16 21:01:56 +0300

    Material ui layout

--- src/Material-UI.js
+++ src/Material-UI.js
@@ -1,9 +1,56 @@
+import { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
 import Drawer from "@material-ui/core/Drawer";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import { styles } from "./material-ui.styles";
 
-const MaterialUI = () => {
-  return <div></div>;
+const useStyles = makeStyles(styles);
+
+const MaterialUI = ({ children }) => {
+  const [isOpened, setIsOpened] = useState(false);
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar className={classes.toolbar}>
+          <IconButton
+            color="inherit"
+            onClick={() => setIsOpened(!isOpened)}
+            className={classes.icon}
+          >
+            {isOpened ? <ChevronLeftIcon /> : <MenuIcon />}
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Header
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+      <div className={classes.container}>
+        <Drawer
+          variant="permanent"
+          classes={{
+            paper: clsx(classes.drawer, {
+              [classes.paper]: !isOpened,
+              [classes.opened]: isOpened,
+            }),
+          }}
+        >
+          Drawer
+        </Drawer>
+        <main className={classes.main}>{children}</main>
+      </div>
+      <div className={classes.footer}>
+        <Typography variant="h6">Footer</Typography>
+      </div>
+    </div>
+  );
 };
 
 export default MaterialUI;
--- src/PureReact.js
+++ src/PureReact.js
@@ -1,29 +1,21 @@
 import { useState } from "react";
 import MenuIcon from "@material-ui/icons/Menu";
-import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import "./pure-react.css";
 
-const PureReact = () => {
+const PureReact = ({ children }) => {
   const [isOpened, setIsOpened] = useState(false);
   return (
     <div className="App">
       <div className="header">
         <div className="icon" onClick={() => setIsOpened(!isOpened)}>
-          {isOpened ? <ArrowBackIcon /> : <MenuIcon />}
+          {isOpened ? <ChevronLeftIcon /> : <MenuIcon />}
         </div>
         <div className="header-title">Header</div>
       </div>
       <div className="container">
         <aside className={`${isOpened ? "opened" : ""} drawer`}>Drawer</aside>
-        <main className="main">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum
-        </main>
+        <main className="main">{children}</main>
       </div>
       <div className="footer">Footer</div>
     </div>
