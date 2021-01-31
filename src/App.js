fd59d728e68dc74c75c59c50cb19ba9e90fe5f43
KaterinaLupacheva <katerinalupacheva@gmail.com>
2021-01-31 15:46:14 +0300

    Master branch cleanup

--- src/App.js
+++ src/App.js
@@ -1,65 +1,8 @@
-import PureReact from "./PureReact";
 import MaterialUI from "./Material-UI";
 
 function App() {
   return (
-    // <MaterialUI>
-    // <p>
-    //   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-    //   tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-    //   veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-    //   commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-    //   velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-    //   occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-    //   mollit anim id est laborum
-    // </p>
-    // <p>
-    //   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-    //   tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-    //   veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-    //   commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-    //   velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-    //   occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-    //   mollit anim id est laborum
-    // </p>
-    // <p>
-    //   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-    //   tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-    //   veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-    //   commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-    //   velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-    //   occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-    //   mollit anim id est laborum
-    // </p>
-    // <p>
-    //   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-    //   tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-    //   veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-    //   commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-    //   velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-    //   occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-    //   mollit anim id est laborum
-    // </p>
-    // <p>
-    //   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-    //   tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-    //   veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-    //   commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-    //   velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-    //   occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-    //   mollit anim id est laborum
-    // </p>
-    // <p>
-    //   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-    //   tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-    //   veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-    //   commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-    //   velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-    //   occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-    //   mollit anim id est laborum
-    // </p>
-    // </MaterialUI>
-    <PureReact>
+    <MaterialUI>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
@@ -87,7 +30,34 @@ function App() {
         occaecat cupidatat non proident, sunt in culpa qui officia deserunt
         mollit anim id est laborum
       </p>
-    </PureReact>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum
+      </p>
+    </MaterialUI>
   );
 }
 
