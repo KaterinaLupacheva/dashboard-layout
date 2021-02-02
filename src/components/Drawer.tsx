bf537948f41b56b2931aae54a33e545d82b0eca0
KaterinaLupacheva <katerinalupacheva@gmail.com>
2021-02-02 13:01:18 +0300

    Mini drawer

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -4,17 +4,25 @@ import clsx from "clsx";
 import { makeStyles, Theme } from "@material-ui/core/styles";
 import { useDrawerContext } from "../contexts/drawer-context";
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme: Theme) => ({
   drawer: {
     background: "#D8DCD6",
     position: "static",
-    transition: "width .7s",
   },
   closed: {
-    width: "0px",
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    width: theme.spacing(7) + 1,
+    overflowX: "hidden",
   },
   opened: {
     width: "240px",
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
   },
 }));
 
